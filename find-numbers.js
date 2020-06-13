a41e9e2e55318cda91b8e297454ccef3c531b44c
Ed <edwoulfe@gmail.com>
2020-06-12 19:11:47 -0500

    Easy problem dealing with the number of digits in a number. Looped through the array, turned each number into a string and check if the module of that number resulted in a 0, if it does then it has an even number of digits and add 1 to the counter variable.

---
+++ find-numbers.js
@@ -0,0 +1,14 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+
+function findNumbers(nums) {
+    let counter = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 === 0) {
+            counter++;
+        }
+    }
+    return counter;
+};
+
+console.log(findNumbers([555, 901, 482, 1771]));
